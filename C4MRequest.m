2ba0d6a7787f766bf550e653eb39780b61b9cca6
Damien Praca <damien.praca@c4mprod.com>
2012-06-25 15:40:04 +0200

    license update

--- C4MRequest.m
+++ C4MRequest.m
@@ -8,7 +8,7 @@
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * C4MAndroidImageManager is distributed in the hope that it will be useful,
+ * C4MiOS_RequestManager is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
